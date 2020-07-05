9dab5ec8643e57d69ac7022bf042da164f051928
gopitha <gopitha@live.no>
2020-07-05 22:08:00 +0200

    Added a new class Battele

--- EFSamurai.Domain/Class1.cs
+++ EFSamurai.Domain/Class1.cs
@@ -32,5 +32,15 @@ namespace EFSamurai.Domain
 
         public Samurai Samurai { get; set; }
     }
+
+    public class Battele
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsBrutal { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
 }
 
