957ae9d4345cef11b24f9e53b7ec51f6055c52bc
gopitha <gopitha@live.no>
2020-07-05 22:49:58 +0200

    Added a method named: AddOneSamurai

--- EFSamurai.App/Program.cs
+++ EFSamurai.App/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using EFSamurai.Data;
+using EFSamurai.Domain;
 
 namespace EFSamurai.App
 {
     class Program
     {
+        private static void AddOneSamurai()
+        {
+            var samurai = new Samurai {Name = "Zelda"};
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
