45a554125d356d86cd8429b4838238eaa05eaece
gopitha <gopitha@live.no>
2020-07-05 20:51:01 +0200

    Added the class Samurai in Domain project, with Id prop. Added the class SamuraiContext in Data project.

--- EFSamurai.Data/Class1.cs
+++ EFSamurai.Data/Class1.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EFSamurai.Domain;
 
 namespace EFSamurai.Data
 {
-    public class Class1
+    public class SamuraiContext :DbContext
+
     {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder opitionsBuilder)
+        {
+            opitionsBuilder.UseSqlServer(
+                @"Server = (localdb)\MSSQLLocalDB;) " +
+                @"Database=EFSamurai; " +
+                @"Trusted_Connection = True; " );
+        }
     }
 }
