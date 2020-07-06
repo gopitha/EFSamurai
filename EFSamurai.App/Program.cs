16462fc3163878538d6f161c2d8bac06ba6e6add
gopitha <gopitha@live.no>
2020-07-06 12:33:58 +0200

    Added a method named AddSomeBattles and added some data.

--- EFSamurai.App/Program.cs
+++ EFSamurai.App/Program.cs
@@ -19,7 +19,7 @@ namespace EFSamurai.App
 
         public static void AddSomeSamurais()
         {
-            IList<Samurai> SamuraiList = new List<Samurai>()
+            IList<Samurai> samuraiList = new List<Samurai>()
             {
                 new Samurai() {Name = "Yukio"}, 
                 new Samurai() {Name = "Renzo"},
@@ -28,13 +28,59 @@ namespace EFSamurai.App
 
             using (var context = new SamuraiContext())
             {
-                context.Samurais.AddRange(new List<Samurai>());
+                context.Samurais.AddRange(samuraiList);
                 context.SaveChanges();
             }
 
         }
 
+        public static void AddSomeBattles()
+        {
+            IList<Battle> BattleList = new List<Battle>()
+            {
+                new Battle()
+                {
+                    Name = "Battle 1",
+                    Description = "Game of the Battle 1",
+                    IsBrutal = true,
+                    StartDate = new DateTime(2020,08,01),
+                    EndDate = new DateTime(2020,08,02), 
+                    BattleLog = new BattleLog()
+                    {
+                        Name = "BattleLog for Battle 1",
+                        BattleEvents = new List<BattleEvent>()
+                        { new BattleEvent
+                            {
+                        Summary = "Summary BattleEvent 1",
+                        Description= "Description BattleEvent 1",
+                        Order= 1
+                            },
+                        new BattleEvent
+                        {
+                            Summary = "Summary BattleEvent 1.2",
+                            Description = "Description BattleEvent 1.2",
+                            Order=2
+                        },
+                        new BattleEvent
+                        {
+                            Summary  ="Summary BattleEvent 1.3",
+                            Description = "Description BattleEvent 1.3",
+                            Order =3
+                        }
+                        }
+                    }
 
+                },
+
+                new Battle()
+                {
+                    Name="Battle 2",
+                    Description = 
+                }
+              
+
+            };
+        }
 
         static void Main(string[] args) 
         {
